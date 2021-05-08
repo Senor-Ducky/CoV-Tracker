0fec351266d2d0414fe54b0e24407a01207a9f12
Supreme-Watermelon <robom460@gmail.com>
2021-05-08 15:31:30 +0530

    added favicons and nav-logo

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBiohazard } from '@fortawesome/free-solid-svg-icons';
+
 
 const Navbar = () => {
     return(
         <div id="navbar" className="container-lg">
-            <h1 id="branding">CoV-Tracker</h1>
+            <h1 id="branding"><FontAwesomeIcon icon={faBiohazard} size='1x' />CoV-Tracker</h1>
             <ul id="button-array" className="p-2 flex space-x-4">
                 <li className="flex space-x-4 ">
                     <button id="home-button"  className="bg-gradient-to-r from-purple-400 via-deep purple-300 to-indigo-500 hover:from-blue-400 hover:to-light blue-500 text-white font-bold py-2 px-4 border-b-4 border-indigo-700 hover:border-indigo-500 rounded">
